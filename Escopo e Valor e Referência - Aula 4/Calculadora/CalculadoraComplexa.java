f47e2d058290c50d92881cef1c33a9d515c89eac
Julia Barros <111322420+JulinhaBarros@users.noreply.github.com>
2023-03-31 18:34:15 -0300

    Add files via upload

---
+++ Escopo e Valor e Referência - Aula 4/Calculadora/CalculadoraComplexa.java
@@ -0,0 +1,32 @@
+package Calculadora;
+
+import java.util.Scanner;
+
+public class CalculadoraComplexa {
+
+    Scanner leia = new Scanner(System.in);
+
+    public void interacao(){
+        System.out.println("Seja bem vinde ao nosso sistema de calculo" + "da fórmula de bhaskara");
+        System.out.println("Você DEVERÁ digitar três números (a,b,c) "+ "para que o calculo seja realizado");
+        System.out.println("Por favor, digite o primeiro número (a): ");
+        float a = leia.nextFloat();
+        System.out.println("Por favor, digite o segundo número (b): ");
+        float b = leia.nextFloat();
+        System.out.println("Por favor, digite o terceiro número (c): ");
+        float c = leia.nextFloat();
+
+        System.out.println(bhaskara(a,b,c));
+    }
+    public String bhaskara(float a, float b, float c){
+        // delta = b^2 - 4 . a. c
+        double delta = Math.pow(b,2) - 4 * a * c;
+
+        // (-b +- vdelta) / 2. a
+
+        double x1 = (-b + Math.sqrt(delta)) / 2 * a;
+        double x2 = (-b - Math.sqrt(delta)) / 2 * a;
+
+        return "O delta é: " +delta+ "\nAs raízes da equação são: "+ "X1 =" +x1+ " X2 = " +x2;
+    }
+}
